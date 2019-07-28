498f437e7eba739d02b61d2beaded5c1e6f0e14d
quincy <miaoqing2014@163.com>
2019-07-28 15:26:21 +0800

    枚举类

---
+++ base_java/src/main/java/com/quincy/java/base/enumdemo/Color.java
@@ -0,0 +1,9 @@
+package com.quincy.java.base.enumdemo;
+
+/**
+ * author:quincy
+ * Date:2019-07-28
+ */
+public enum  Color {
+    BLACK,WHITE
+}
---
+++ base_java/src/main/java/com/quincy/java/base/enumdemo/Laguage.java
@@ -0,0 +1,13 @@
+package com.quincy.java.base.enumdemo;
+
+/**
+ * author:quincy
+ * Date:2019-07-28
+ */
+public enum  Laguage {
+    JAVA("世界上最好的语言","适用于企业级开发");
+
+    Laguage(String s, String s1) {
+
+    }
+}
