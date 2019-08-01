766ff19a5a6db6af240f8f87747f4488ab564908
quincy <miaoqing2014@163.com>
2019-08-01 16:19:09 +0800

    枚举类

--- base_java/src/main/java/com/quincy/java/base/equals/EqualsDriver.java
+++ base_java/src/main/java/com/quincy/java/base/equals/EqualsDriver.java
@@ -1,5 +1,9 @@
 package com.quincy.java.base.equals;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * author:quincy
  * Date:2019-07-26
@@ -8,5 +12,9 @@ public class EqualsDriver {
     public static void main(String[] args) {
         System.out.println();
 
+
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("888");
+        List<String> list = Collections.synchronizedList(arrayList);
     }
 }
---
+++ base_java/src/main/java/com/quincy/java/base/manythreadarraylist/ArrayListDriver.java
@@ -0,0 +1,45 @@
+package com.quincy.java.base.manythreadarraylist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * author:quincy
+ * Date:2019-08-01
+ */
+public class ArrayListDriver {
+    public static void main(String[] args) {
+
+    }
+
+
+    private void arraylistTest(){
+        List<String> list = new ArrayList<>();
+        runThread(list);
+    }
+
+    private void synchronizedListTest(){
+        List<String> list = Collections.synchronizedList(new ArrayList<>());
+        runThread(list);
+    }
+
+    private void copyOnWriteArrayListTest(){
+        CopyOnWriteArrayList<String> list = new CopyOnWriteArrayList<>();
+        runThread(list);
+    }
+
+    private void runThread(List list){
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    list.add(UUID.randomUUID().toString().substring(0,8));
+                    System.out.println(list);
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
