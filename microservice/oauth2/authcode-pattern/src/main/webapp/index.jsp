ae3e3741f833c72ae9bdb3299bb6186f847e3933
quincy <miaoqing2014@163.com>
2019-08-17 13:29:54 +0800

    删除多余的文件

---
+++ microservice/oauth2/authcode-pattern/src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+</body>
+
+<form action="" method="post">
+  用户名  <input type="text">
+    密码  <input type="text">
+</form>
+
+</html>
---
+++ microservice/oauth2/implicit-pattern/src/main/java/com/quincy/implicit/authorizationserver/OAuth2AuthorizationServer.java
@@ -0,0 +1,27 @@
+package com.quincy.implicit.authorizationserver;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+//授权服务器配置
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends
+        AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients)
+            throws Exception {
+        clients.inMemory()
+            .withClient("clientapp")
+            .secret("112233")
+            .redirectUris("http://localhost:8080/callback")
+            // 简化模式
+            .authorizedGrantTypes("implicit")
+            .accessTokenValiditySeconds(120)
+            .scopes("read_userinfo", "read_contacts");
+    }
+
+}
