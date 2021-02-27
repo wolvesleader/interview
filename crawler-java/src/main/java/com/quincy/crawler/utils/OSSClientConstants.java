5627b98cf5c23ad82a5f05fd3d58fa70ce89adc9
wolvesleader <aini99.commiao>
2021-02-27 09:41:11 +0800

    爬虫代码修改

--- crawler-java/src/main/java/com/quincy/crawler/utils/OSSClientConstants.java
+++ crawler-java/src/main/java/com/quincy/crawler/utils/OSSClientConstants.java
@@ -11,9 +11,9 @@ public class OSSClientConstants {
     //视频服务器接口地址
     public static final String VIDEOENDPOINT = "oss-cn-beijing.aliyuncs.com";
     //阿里云API的密钥Access Key ID
-    public static final String ACCESSKEYID = "LTAI4FqYgA2fp9e7wWVfFpWE";
+    public static final String ACCESSKEYID = "";
     //阿里云API的密钥Access Key Secret
-    public static final String ACCESSKEYSECRET = "OWv6cAdpCgqlBGocFMuMMd7E3BrchA";
+    public static final String ACCESSKEYSECRET = "";
     //文件存储bucket名称
     public static final String PICTUREBUCKET = "exchangebook";
     //视频存储bucket名称
